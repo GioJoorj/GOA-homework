5e47b952dd133fb270be9b1e1ada995a80a725b2
GioJoorj <giorgikaadze7@gmail.com>
2025-07-01 22:25:35 +0400

    GOA

---
+++ day 20/classwork/lesson01.py
@@ -0,0 +1,5 @@
+name = float(input("enter any number :"))
+if name < 20:
+    print("you will be married at age that you entered")
+else:
+    print("you are not ready for tolma")
---
+++ day 20/classwork/lesson02.py
@@ -0,0 +1,3 @@
+name = int(input("enter number:"))
+for i in range(name,0,-1):
+    print(i)
