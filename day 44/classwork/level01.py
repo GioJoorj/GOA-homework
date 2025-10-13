72fa7181f3ebcb6bac31e2a7aff32ba218cfa652
GioJoorj <giorgikaadze7@gmail.com>
2025-10-13 18:45:11 -0400

    GOA

---
+++ day 44/classwork/level01.py
@@ -0,0 +1,6 @@
+#დაწერეთ manual_sum ფუნქცია არ გამოიყენეოთ sum() ფუნქცია
+def manual_sum():
+    user1 = int(input("enter number:"))
+    user2 = int(input("enter second number:"))
+    print(user1 + user2)
+manual_sum()
---
+++ day 50/classwork/level02.py
@@ -0,0 +1,7 @@
+for i in():
+    password = "123"
+    user = input('enter password')
+    if password == user:
+        print("Correct")
+    else:
+        print("Try again")
